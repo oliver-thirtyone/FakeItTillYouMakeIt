816470d56773e2538846f12c57dda8937a54d4a9
Oliver S <r3zn1k@bluewin.ch>
2020-10-16 17:40:57 +0200

    Make a clear start for C#

--- CSharp/FizzBuzzTest.cs
+++ CSharp/FizzBuzzTest.cs
@@ -10,25 +10,19 @@ namespace FakeItTillYouMakeIt
         [Test]
         public void TestFizzBuzz()
         {
+            // TODO: Delete this after the first tun
+            // ---------------------------------------------------------------------------------------------------------
             var java = PathUtilities.LocateFileFromEnvironmentPath("javaw.exe");
             var counterDisplayJar = PathUtilities.GetAdjacentFile("CounterDisplay.jar");
-
             TestCounter.ResetAndLaunch(java.FirstOrDefault(), counterDisplayJar); // Comment out after 1st run
+            // ---------------------------------------------------------------------------------------------------------
+
             TestCounter.Track(Check20);
         }
 
         private static void Check20()
         {
-            const string expected = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
-            Assert.AreEqual(expected, FizzBuzz.Print(20));
-        }
-    }
-
-    public static class FizzBuzz
-    {
-        public static string Print(int top)
-        {
-            return "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+            Assert.That("expected", Is.EqualTo("expected"));
         }
     }
 }
